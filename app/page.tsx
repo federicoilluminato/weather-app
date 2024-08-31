0ecbda4694f525152dfc6b2556c0c29972d5cec5
federico <federicoilluminato@hotmail.com>
2024-08-31 15:29:42 -0300

    basic UI

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
-import Image from "next/image";
+import Input from "./component/Input";
 
 export default function Home() {
   return (
-    <main className="">
-      <div>
-        page
+    <main className="bg-cover bg-gradient-to-r from-blue-500 to-blue-300 h-screen">
+      <div className="bg-white/25 w-full flex flex-col h-full">
+        <div className="flex flex-col md:flex-row justify-between items-center p-12">
+          <Input/>
+          <h1>Weather App</h1>
+        </div>
       </div>
     </main>
   );
