e99f019478718c9546df9db74660008853ce6497
federico <federicoilluminato@hotmail.com>
2024-08-31 15:53:40 -0300

    gitignore .env

--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,10 @@ import Input from "./component/Input";
 export default function Home() {
   return (
     <main className="bg-cover bg-gradient-to-r from-blue-500 to-blue-300 h-screen">
-      <div className="bg-white/25 w-full flex flex-col h-full">
+      <div className="bg-white/25 w-full flex flex-col h-fit">
         <div className="flex flex-col md:flex-row justify-between items-center p-12">
           <Input/>
-          <h1>Weather App</h1>
+          <h1 className="mb-8 md:mb-0 order-1 text-white py-2 px-4 rounded-xl italic font-bold">Weather App</h1>
         </div>
       </div>
     </main>
